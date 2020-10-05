f5a45ba7d6f1d85c3b93c04f7e1fe4cc36b92669
john-hawkins <hawkins.john.c@gmail.com>
2020-10-06 10:09:16 +1100

    Processing references and POS labels

--- data/prepare_dict.py
+++ data/prepare_dict.py
@@ -13,9 +13,10 @@ end_string ="End of the Project Gutenberg EBook"
 def initialise_new_record(content):
     global CURRENT_KEY
     global antonyms_started
-    CURRENT_KEY = content 
+    mykey, ref, pos = extract_ref_or_pos(content)
+    CURRENT_KEY = mykey 
     antonyms_started = False
-    dictionary[CURRENT_KEY] = {"SYN":[],"ANT":[]}
+    dictionary[CURRENT_KEY] = {"SYN":[],"ANT":[],"REF":ref, "POS":pos}
 
 ##################################################################
 def finalise():
@@ -26,7 +27,8 @@ def finalise():
 def clean(x):
     temp = re.sub('\{\[(.*)\]\?\}', '\\1', x)    
     temp2 = re.sub('\.','',temp)
-    return temp2
+    temp3 = re.sub('=','',temp2)
+    return temp3
 
 ##################################################################
 def update_content(starter, content, stripped_line):
@@ -63,6 +65,28 @@ def update_dictionary(content,section):
     record[section] = curr
     dictionary[CURRENT_KEY] = record
 
+##################################################################
+def extract_ref_or_pos(content):
+    pos = ""
+    ref = ""
+    res = re.findall(r"\\[a-zA-Z]+\.\\", content)
+    if len(res) != 0:
+        pos = res[0]
+    temp = re.sub(r"\\[a-zA-Z]+\.\\", '', content).strip()
+    res = re.findall(r"\[.*\]", temp)
+    if len(res) != 0:
+        ref = res[0]
+        ref = re.sub(r"\[see", '', ref, flags=re.IGNORECASE).strip()
+        ref = re.sub(r"\]", '', ref).strip()
+    new_key = re.sub(r"\[.*\]", '', temp).strip()
+    if len(new_key) == 0:
+        new_key = ref
+    return new_key, ref, pos
+
+
+##################################################################
+def replace_refs(content):
+    return re.sub(r"\[see (.*)\]", '\\1', content, flags=re.IGNORECASE).strip()
 
 ##################################################################
 
@@ -75,8 +99,10 @@ with open(dict_file, "r") as f:
         content = stripped_line[5:]
         if starter == "KEY:":
             process_started = True
-            initialise_new_record(content)
+            initialise_new_record(content.lower())
         elif process_started:
+            content = replace_refs(content).lower()
+            stripped_line = replace_refs(stripped_line).lower()
             update_content(starter, content, stripped_line)
     # NOW SAVE THE GENERATED DATA STRUCTURE
     finalise()
